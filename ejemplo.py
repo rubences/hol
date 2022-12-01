bb234258805f34ac35803bb1f61e3a081e9e1517
rubences <91731294+rubences@users.noreply.github.com>
2022-12-01 18:28:15 +0000

    cambios

--- ejemplo.py
+++ ejemplo.py
@@ -1,10 +1,13 @@
 #!/bin/python3
 
+from __future__ import annotations
+from collections.abc import Iterable
 import math
 import os
 import random
 import re
 import sys
+
 
 # Complete the birthdayCakeCandles function below.
 def birthdayCakeCandles(ar):
@@ -20,14 +23,7 @@ def birthdayCakeCandles(ar):
 
 
 if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+import doctest
 
-    ar_count = int(input())
-
-    ar = list(map(int, input().rstrip().split()))
-
-    result = birthdayCakeCandles(ar)
-
-    fptr.write(str(result) + '\n')
-
-    fptr.close()
+    # run doc test
+    doctest.testmod()
